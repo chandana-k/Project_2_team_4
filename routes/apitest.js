97e140dfb269b8cfd35bc3aaa9e968d7f391dd0a
thelaceyedit <thelaceyedit@gmail.com>
2018-09-27 19:38:07 -0400

    adding post method

--- routes/apitest.js
+++ routes/apitest.js
@@ -1,15 +1,20 @@
 // require api via request 
 
+
+// require api keys
+
     var keys = require("../public/keys.js");
+    console.log("keys", keys);
+ 
 
-        keys.find({  }, function(err, res) {
+        // keys.find({}, function(err, res) {
 
-            if (err) {
-                console.log(err);
-            }
-            console.log(JSON.stringify(res, null, 2));
+        //     if (err) {
+        //         console.log(err);
+        //     }
+        //     console.log(JSON.stringify(res, null, 2));
 
-        });
+        // });
 
 module.exports = function(app) {
     var request = new XMLHttpRequest();
@@ -24,6 +29,8 @@ module.exports = function(app) {
         ETSY_SECRET: process.env.ETSY_SECRET,
         ETSY_ID: process.env.ETSY_ID
     });
+    console.log("here are the keys");
+    console.log(process.env.ETSY_SECRET);
 
       if (request.status >= 200 && request.status < 400) {
         data.forEach(request => {
@@ -34,24 +41,24 @@ module.exports = function(app) {
       }
     }
     
-    request.send();
+    request.send(data);
 };
 
 
 
-// module.exports = function(app) {
-//  app.get("/", function(req, res) {
-
-//      var searchButton = function() {
-//             var request = require("request");
+module.exports = function(app) {
+ app.get("/etsy", function(req, res) {
+    console.log("etsy route is hit");
+    //  var searchButton = function() {
+    //         var request = require("request");
             
-//             request({
-//                 ETSY_SECRET: process.env.ETSY_SECRET,
-//                 ETSY_ID: process.env.ETSY_ID
-//             });
-//         };
+    //         request({
+    //             ETSY_SECRET: process.env.ETSY_SECRET,
+    //             ETSY_ID: process.env.ETSY_ID
+    //         });
+    //     };
 
-//         res.sendFile(path.join(__dirname, "../public/example.html"));
-//         searchButton();
-//     });
-// };
+    //     res.sendFile(path.join(__dirname, "../public/example.html"));
+    //     searchButton();
+    });
+};
