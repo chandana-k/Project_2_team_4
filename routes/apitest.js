42a07cee9bfb047255530988885d49632edd38fd
thelaceyedit <thelaceyedit@gmail.com>
2018-09-26 21:17:52 -0400

    adding functioning api route

---
+++ routes/apitest.js
@@ -0,0 +1,29 @@
+// require api via request 
+
+var keys = require("../public/keys.js");
+
+    keys.find({  }, function(err, res) {
+
+        if (err) {
+            console.log(err);
+        }
+        console.log(JSON.stringify(res, null, 2));
+
+    });
+
+
+module.exports = function(app) {
+
+    app.get("/", function(req, res) {
+        var searchButton = function() {
+            var request = require("request");
+            
+            request ({
+                ETSY_ID: process.env.ETSY_ID,
+                ETSY_SECRET: process.env.ETSY_SECRET
+            });
+        };
+
+        res.sendFile(path.join(__dirname, "../public/example.html"));
+    });
+};
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 
+require("dotenv").config();
+
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
