4f23fcb777522d09e7cc65d3d99cb6cd56ee3129
thelaceyedit <thelaceyedit@gmail.com>
2018-09-27 18:18:44 -0400

    cors issue

--- routes/apitest.js
+++ routes/apitest.js
@@ -1,48 +1,57 @@
 // require api via request 
 
-var keys = require("../public/keys.js");
+    var keys = require("../public/keys.js");
 
-    keys.find({  }, function(err, res) {
+        keys.find({  }, function(err, res) {
 
-        if (err) {
-            console.log(err);
-        }
-        console.log(JSON.stringify(res, null, 2));
+            if (err) {
+                console.log(err);
+            }
+            console.log(JSON.stringify(res, null, 2));
 
-    });
-
-
-
-
+        });
 
 module.exports = function(app) {
- app.get("/", function(req, res) {
+    var request = new XMLHttpRequest();
 
-     var searchButton = function() {
-            var request = require("request");
-            
-            request({
-                ETSY_SECRET: process.env.ETSY_SECRET,
-                ETSY_ID: process.env.ETSY_ID
-            });
+    request.open('GET', "https://openapi.etsy.com/v2/listings/active.js?keywords=soap&limit=12&includes=Images:1&api_key=a2p83t9puv67kp0dnvsd4yka", true);
+    request.onload = function () {
+    
+      // access JSON data here
+      var data = JSON.parse(this.response);
 
-            // request ({
-            //     EBAY_SECRET: process.env.ETSY_SECRET,
-            //     EBAY_ID: process.env.EBAY_ID,
-            //     EBAY_DEV_ID: process.env.EBAY_DEV_ID
-            // });
-        
-
-
-            // params ={
-            //     appid: ("EBAY_ID),
-            //     version: ("967"),
-            //     siteid: ("0")
-            //     };
-
-        };
-
-        res.sendFile(path.join(__dirname, "../public/example.html"));
-        searchButton();
+      request({
+        ETSY_SECRET: process.env.ETSY_SECRET,
+        ETSY_ID: process.env.ETSY_ID
     });
+
+      if (request.status >= 200 && request.status < 400) {
+        data.forEach(request => {
+          console.log(request);
+        });
+      } else {
+        console.log('error');
+      }
+    }
+    
+    request.send();
 };
+
+
+
+// module.exports = function(app) {
+//  app.get("/", function(req, res) {
+
+//      var searchButton = function() {
+//             var request = require("request");
+            
+//             request({
+//                 ETSY_SECRET: process.env.ETSY_SECRET,
+//                 ETSY_ID: process.env.ETSY_ID
+//             });
+//         };
+
+//         res.sendFile(path.join(__dirname, "../public/example.html"));
+//         searchButton();
+//     });
+// };
