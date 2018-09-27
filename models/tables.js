ee866a905be0a4dac54730f6a078fce2db0cadfc
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-09-27 18:45:57 -0400

    #62 #63 add columns to DB tables

--- models/tables.js
+++ models/tables.js
@@ -5,7 +5,8 @@ module.exports = function (sequelize, DataTypes) {
     tableName: {
       type: DataTypes.STRING,
       allowNull: false,
-    }
+    },
+    eventDate: DataTypes.Date
   });
 
   Table.associate = function (models) {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -57,7 +57,7 @@ module.exports = function (app) {
   });
 
   function createGeneralTable(uname, tabName) {
-    db.sequelize.query("CREATE TABLE " + tabName + " (id INT not null auto_increment, itemName VARCHAR(255) not null UNIQUE, itemUrl varchar(255) not null, public BOOLEAN default false, primary key (id));").then(function (resp) {
+    db.sequelize.query("CREATE TABLE " + tabName + " (id INT not null auto_increment, itemName VARCHAR(255) not null UNIQUE, itemUrl varchar(255) not null, public BOOLEAN default false, userShopped INT, primary key (id));").then(function (resp) {
       console.log(resp);
       db.User.findOne({
         where: {
