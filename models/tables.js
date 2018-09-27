faa44ce184f22ba9fca41e8cb23592948315b3d6
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-09-27 19:25:30 -0400

    Fix DataType typo

--- models/tables.js
+++ models/tables.js
@@ -6,7 +6,7 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    eventDate: DataTypes.Date
+    eventDate: DataTypes.DATE
   });
 
   Table.associate = function (models) {
