434f950dcb854376ff23c2cb04e3811925a7e00a
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-09-28 17:03:03 -0400

    Working to migrate Etsy request to backend

--- public/js/search.js
+++ public/js/search.js
@@ -1,23 +1,18 @@
 
-$("#searchButton").on("click", function (event) {
-  event.preventDefault();
+// Hit back end to get Etsy data
 
-  // grab text from search box
-  var search = $("#searchVal").val();
-  console.log("Searching for: " + search);
-
-  //  construct our URL
-  // var queryURL = "https://openapi.etsy.com/v2/listings/trending?api_key=a2p83t9puv67kp0dnvsd4yka";
-  //    https://openapi.etsy.com/v2/users/etsystore?api_key=a2p83t9puv67kp0dnvsd4yka
-  // https://urlreq.appspot.com/req?method=GET&url=https://www.etsy.com/?=" + search + "&api_key=a2p83t9puv67kp0dnvsd4yka
-  // this one works
-  // https://openapi.etsy.com/v2/listings/trending?api_key=a2p83t9puv67kp0dnvsd4yka
-
-  $.post({
-    url: "/api/search",
-    data: search,
-  }).then(function (response) {
-    console.log(response);
+$(function () {
+  $('#etsy-search').on('submit', function (event) {
+    event.preventDefault();
+    terms = $('#etsy-terms').val();
+    console.log(terms);
+    $('<p></p>').text('Searching for ' + terms).appendTo('#etsy-images');
+    $.ajax({
+      method: "POST",
+      url: "/api/search",
+      data: terms,
+    }).then(function (response) {
+      console.log(response);
+    });
   });
-
 });
