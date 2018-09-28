5090c989f622f0adc1a21f98c5067c6cbfccd29d
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-09-28 17:35:53 -0400

    Attempt to re-render page with partials...

--- public/js/search.js
+++ public/js/search.js
@@ -1,18 +1,21 @@
+// Author: Karsten
 
 // Hit back end to get Etsy data
-
 $(function () {
   $('#etsy-search').on('submit', function (event) {
     event.preventDefault();
-    terms = $('#etsy-terms').val();
+    terms = $('#etsy-terms').val().trim();
     console.log(terms);
-    $('<p></p>').text('Searching for ' + terms).appendTo('#etsy-images');
-    $.ajax({
-      method: "POST",
-      url: "/api/search",
-      data: terms,
-    }).then(function (response) {
-      console.log(response);
-    });
+    // Validate not empty
+    if (terms.length !== 0) {
+      $('<p></p>').text('Searching for ' + terms).appendTo('#etsy-images');
+      $.ajax({
+        method: "POST",
+        url: "/api/search",
+        data: terms,
+      }).then(function (response) {
+        console.log(response);
+      });
+    }
   });
 });
