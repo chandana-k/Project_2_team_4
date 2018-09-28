4c09c4ab0b8f689fbb2f75b098a18a793b4d2b9b
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-09-27 20:44:12 -0400

    Remove controller lines, start API

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,57 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.User.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+module.exports = function (app) {
+
+  // Get all users
+  app.get("/users", function (req, res) {
+    db.User.findAll({}).then(function (resp) {
+      res.json(resp);
     });
   });
 
-  // Get an example by id
-  app.get("/api/examples/:id", function(req, res) {
-    db.Example.findOne({where: {id: req.params.id}}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get a user by ID
+  app.get("/users/:id", function (req, res) {
+    var id = req.params.id;
+    db.User.findOne({
+      where:
+      {
+        id: id
+      }
+    }).then(function (resp) {
+      res.json(resp);
     });
+  });
+
+  // Get all table names
+  app.get("/tables", function (req, res) {
+    db.Table.findAll({}).then(function (resp) {
+      res.json(resp);
+    });
+  });
+
+  // Get a specific table by name
+  app.get("/tables/name/:name", function (req, res) {
+    var tName = req.params.name;
+    console.log(tName);
+    console.log(db);
+    if (db[tName]){
+
+    }
+    // db[tName].findAll({}).then(function (resp) {
+    //   res.json(resp);
+    // });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
